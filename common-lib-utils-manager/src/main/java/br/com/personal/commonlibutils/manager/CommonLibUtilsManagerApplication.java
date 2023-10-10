5c3ae330dd7b8fad48e0d65e75bc7a22c55a1bad
Robin Bray Morin <robinmorin79@gmail.com>
2023-10-09 22:16:56 -0300

    Adding module manager

---
+++ common-lib-utils-manager/src/main/java/br/com/personal/commonlibutils/manager/CommonLibUtilsManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.personal.commonlibutils.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonLibUtilsManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonLibUtilsManagerApplication.class, args);
+    }
+
+}
